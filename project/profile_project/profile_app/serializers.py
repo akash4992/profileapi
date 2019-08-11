f6a407713e83309c287c0a9cb83e3874e752e498
akash4992 <akashdhiman920@gmail.com>
2019-08-11 15:53:54 +0530

    code is chagne

---
+++ project/profile_project/profile_app/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class HelloSerializer(serializers.Serializer):
+    """Serializes a name field for testing our APIView."""
+    name = serializers.CharField(max_length=10)
+
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserProfile
+        fields = ('id','email','first_name','last_name','password')
+
+        extra_kwargs = {'passwotd':{'write_only':True}}
+
+    def create(self, validated_data):
+        user = models.UserProfile(
+            email = validated_data['email'],
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return  user
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id','user_profile','status_text','created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
